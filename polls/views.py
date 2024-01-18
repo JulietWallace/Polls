ba54db8b28fbce6bbc4322203e623de3f37929ce
Juliet Wallace <2760588w@student.gla.ac.uk>
2024-01-18 22:04:09 +0000

    added index.html

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_data")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def results(request, question_id):
     response="You're looking at the results of question %s."
